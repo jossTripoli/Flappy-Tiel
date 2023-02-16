e70fba055d6ad67d5c70b0bf7217ddcd0701e6f9
Joss <78896064+jossTripoli@users.noreply.github.com>
2023-02-16 15:40:29 -0500

    added audio

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -9,12 +9,14 @@ public class LogicScript : MonoBehaviour
     public int playerScore;
     public Text scoreText;
     public GameObject gameOverScreen;
+    public AudioSource dinkSFX;
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
     {
         playerScore = playerScore + scoreToAdd;
-        scoreText.text = playerScore.ToString(); 
+        scoreText.text = playerScore.ToString();
+        dinkSFX.Play();
     }
 
     public void restartGame() 
